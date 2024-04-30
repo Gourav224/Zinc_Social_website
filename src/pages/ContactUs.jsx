6718cc0500bd2e26e66d0bb2fd174e58d98f6859
Gourav224 <gkrdhiman@gmail.com>
2024-04-30 10:57:40 +0530

    firebase setup completed

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -3,20 +3,43 @@ import MailInput from "../components/MailInput";
 import Button from "../components/Button";
 import emailSvg from "../assets/PhoneEmail.svg";
 import Footer from "../components/Footer";
-
+import { db } from "../utils/FirebaseConfig";
+import { addDoc, collection } from "firebase/firestore";
+import config from "../utils/config";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const ContactUs = () => {
-    const emailRef = useRef(null);
-    const onClick = () => {
-        console.log(emailRef.current.value);
+    const emailRef = useRef("");
+    const emailCollectionRef = collection(db, config.dbName);
+    const onSubmit = async () => {
         if (emailRef.current.value) {
-            alert("Email sent");
-            emailRef.current.value = "";
+            try {
+                const email = emailRef.current.value;
+                await addDoc(emailCollectionRef, { email });
+                emailRef.current.value = "";
+                // Success message
+                toast.success("Email sent successfully!", {
+                    position: "bottom-right",
+                });
+            } catch (error) {
+                // Error message
+                toast.error("Error sending email. Please try again later.", {
+                    position: "bottom-right",
+                });
+            }
         } else {
-            alert("Please enter a valid email");
+            // Empty email error message
+            toast.error("Please enter a valid email!", {
+                position: "bottom-right",
+            });
         }
     };
+
     return (
-        <div id="contact" className="max-h-screen sm:min-h-screen  text-center flex flex-col bg-white">
+        <div
+            id="contact"
+            className="max-h-screen sm:min-h-screen  text-center flex flex-col bg-white"
+        >
             <h2 className="text-customBlue block sm:hidden pt-12 mx-auto text-2xl font-semibold leading-snug">
                 Contact us
             </h2>
@@ -33,11 +56,10 @@ const ContactUs = () => {
                             label="Email"
                             ref={emailRef}
                             placeholder="loremipsum@gmail.com"
-                            className=""
                         />
                         <Button
                             className="text-center mt-2 md:mt-4 w-12 h-6 text-xs  sm:w-16 sm:h-10 rounded-lg   sm:text-xl"
-                            onClick={onClick}
+                            onClick={onSubmit}
                         >
                             Send
                         </Button>
@@ -47,6 +69,7 @@ const ContactUs = () => {
             <div className="w-full">
                 <Footer />
             </div>
+            <ToastContainer />
         </div>
     );
 };
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,11 @@
-const conf = {
-    apiKey: String(import.meta.env.VITE_apiKey,
+const config = {
+    apiKey: String(import.meta.env.VITE_apiKey),
     authDomain: String(import.meta.env.VITE_authDomain),
     projectId: String(import.meta.env.VITE_projectId),
     storageBucket: String(import.meta.env.VITE_storageBucket),
-    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    TinyMCE_APIKey: String(import.meta.env.VITE_TinyMCE_APIKey)
-}
-export default conf;
+    messagingSenderId: String(import.meta.env.VITE_messagingSenderId),
+    appId: String(import.meta.env.VITE_appId),
+    measurementId: String(import.meta.env.VITE_measurementId),
+    dbName: String(import.meta.env.VITE_dbName),
+};
+export default config;
